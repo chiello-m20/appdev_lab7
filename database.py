8f77cd32b30f6b6040aa1afad436261dbf0ff6cc
Chiello Mabano <giedump01@gmail.com>
2025-04-16 15:27:14 +0800

    Updated database.py with new PostgreSQL configuration

--- database.py
+++ database.py
@@ -2,12 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Get DATABASE_URL from environment variables (using the **variable name**)
-database_url = os.environ.get("DATABASE_URL")
-
-# Check if it's None and raise an error to help with debugging
-if not database_url:
-    raise ValueError("DATABASE_URL environment variable is not set!")
+# Get DATABASE_URL from environment variables
+database_url = os.getenv("DATABASE_URL")
 
 # Connect to PostgreSQL
 engine = create_engine(database_url, echo=True)
